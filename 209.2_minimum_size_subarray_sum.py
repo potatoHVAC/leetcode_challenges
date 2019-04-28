c0dc2abbc36e827c3daf40f42606e3b5ead6d739
Daniel Speer <dan.speer11@gmail.com>
2019-04-28 01:09:56 -0700

    add 209.1 and 209.2

---
+++ 209.2_minimum_size_subarray_sum.py
@@ -0,0 +1,34 @@
+# Minimum Size Subarray Sum
+# https://leetcode.com/problems/minimum-size-subarray-sum/
+# Completed 4/28/19
+
+
+def min_subarray(arr, target):
+    '''
+    input:  arr    -> list[int] 
+            target -> int       -- maximum sum
+    output: int                 -- length of minimum subarray
+
+    Returns the length of the minumum subarray with its sum >= target
+    '''
+    if sum(arr) < target: return 0
+
+    left, right = 0, 0
+    subarray_sum = arr[0]
+    subarray_len = len(arr)
+
+    while left < len(arr):
+        if subarray_sum < target:
+            if right == len(arr) - 1: return subarray_len
+            right += 1
+            subarray_sum += arr[right]
+        else:
+            subarray_len = min(subarray_len, right - left + 1)
+            subarray_sum -= arr[left]
+            left += 1
+
+    return subarray_len
+
+class Solution:
+    def minSubArrayLen(self, s: int, nums: List[int]) -> int:
+        return min_subarray(nums, s)
